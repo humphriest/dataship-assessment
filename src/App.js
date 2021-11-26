a61a8e4727c7992514c471d7c17eeb529ace9217
Tim <thumphries@cartrawler.com>
2021-11-26 16:08:39 +0000

    Data ships assessment

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [list, setList] = useState([]);
+  const [newValue, setValue] = useState("");
+
+  const updateText = (event) => {
+    setValue(event.target.value);
+  };
+
+  const addToList = () => {
+    let newList = [...list];
+    const newTask = { name: newValue, done: false };
+    newList.push(newTask);
+    setList(newList);
+    setValue("");
+  };
+
+  const removeItem = (index) => {
+    let newList = [...list];
+    newList.splice(index, 1);
+    setList(newList);
+  };
+
+  const toggleCheckbox = (index) => {
+    let newList = [...list];
+    newList[index].done = !newList[index].done;
+    setList(newList);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <input
+          onChange={updateText}
+          placeholder="add"
+          value={newValue}
+          id="input"
+          aria-label="input"
+        />
+        <button onClick={addToList}>Add to list</button>
+        <ul>
+          {list.map((listItem, i) => (
+            <li className="listItem" key={i}>
+              <input
+                type="checkbox"
+                onChange={() => toggleCheckbox(i)}
+                value={listItem.done}
+              />
+              <div role="definition">{listItem.name}</div>
+              <button aria-label="delete" onClick={() => removeItem(i)}>
+                X
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,46 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen, fireEvent } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+const app = () => {
+  const renderedApp = render(<App />);
+  const input = renderedApp.getByLabelText("input");
+  return {
+    input,
+    ...renderedApp,
+  };
+};
+describe("App Test", () => {
+  beforeEach(() => {
+    const { input } = app();
+    const testString = "Test input";
+    fireEvent.change(input, { target: { value: testString } });
+
+    fireEvent.click(screen.getByText("Add to list"));
+  });
+
+  test("Add to list", () => {
+    const testString = "Test input";
+
+    expect(screen.getByRole("definition")).toHaveTextContent(testString);
+    expect(screen.getByRole("checkbox").value).toBe("false");
+  });
+
+  test("Remove from the list", () => {
+    const testString = "Test input";
+
+    expect(screen.getByRole("definition")).toHaveTextContent(testString);
+    expect(screen.getByRole("checkbox").value).toBe("false");
+
+    fireEvent.click(screen.getByLabelText("delete"));
+    expect(screen.getByRole("list").children).toHaveLength(0);
+  });
+
+  test("Toggle Checkbox", () => {
+    fireEvent.click(screen.getByRole("checkbox"));
+    expect(screen.getByRole("checkbox").value).toBe("true");
+
+    fireEvent.click(screen.getByRole("checkbox"));
+    expect(screen.getByRole("checkbox").value).toBe("false");
+  });
 });
